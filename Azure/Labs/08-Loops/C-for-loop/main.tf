e756f553f021ddd2aa5c9242d8d2ff064f275e66
Syed Imran Nayer <imrannayer@Syeds-MacBook-Pro.local>
2019-12-21 09:37:06 -0600

    updated

--- Azure/Labs/08-Loops/C-for-loop/main.tf
+++ Azure/Labs/08-Loops/C-for-loop/main.tf
@@ -1,12 +1,3 @@
-provider "azurerm" {
-  # subscription_id = "your_subscription_id"
-  # client_id       = "your_appId"
-  # client_secret   = "your_password"
-  # tenant_id       = "your_tenant_id"
-}
-
-
-
 ####### Operations on list 
 
 variable "var_list" {
---
+++ Azure/Labs/08-Loops/C-for-loop/provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  # subscription_id = "your_subscription_id"
+  # client_id       = "your_appId"
+  # client_secret   = "your_password"
+  # tenant_id       = "your_tenant_id"
+}
