b6a2608ed1abf94df8dbb4a825789038f5d49741
Syed Imran Nayer <imrannayer@Syeds-MacBook-Pro.local>
2019-12-27 13:57:10 -0600

    updated CF template

---
+++ AWS/Labs/06-modules/registry/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key" 
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-registry-vpc"
+  cidr = "10.10.0.0/16"
+
+  # azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Environment = "dev"
+  }
+}
+output myvpi {
+  value = module.vpc
+}
