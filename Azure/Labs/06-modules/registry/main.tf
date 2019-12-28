a68dc876bf8656d099b63b4de375f66a63cf270f
Syed Imran Nayer <imrannayer@Syeds-MacBook-Pro.local>
2019-12-27 20:33:34 -0600

    updated TF registry Labs

--- Azure/Labs/06-modules/registry/main.tf
+++ Azure/Labs/06-modules/registry/main.tf
@@ -10,16 +10,12 @@ resource "azurerm_resource_group" "rg" {
   location = "southcentralus"
 }
 
-module "vnet" {
-    source              = "Azure/vnet/azurerm"
-    resource_group_name = "my-tf-rg"
-    location            = "westus"
+module "network" {
+    source              = "Azure/network/azurerm"
+    version             = "2.0.0"
+    location            = "southcentralus"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
     address_space       = "10.0.0.0/16"
-    subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-    tags                = {
-                            environment = "dev"
-                            costcenter  = "it"
-                          }
+    subnet_names        = ["mySubnet"]
+    subnet_prefixes     = ["10.0.1.0/24"]
 }
