96dbebde7048c8eda6dc673d2b534f4635e92efb
Syed I Nayer <imrannayer@users.noreply.github.com>
2020-07-29 09:06:00 -0500

    added module loops lab

--- GCP/Labs/08-Loops/E-Module-for_each/my-1-resource/main.tf
+++ GCP/Labs/08-Loops/E-Module-for_each/my-1-resource/main.tf
@@ -7,14 +7,22 @@ provider "google" {
   # region      = "us-central1"
 }
 
+### example without loop
+# module "myresource-1" {
+#   source        = "../modules"
+#   net_prefix    = "my-first"
+# }
 # module "myresource-2" {
 #   source        = "../modules"
 #   net_prefix    = "my-second"
 # }
 
+
 variable vpc_prefixes {
   default = ["my-first", "my-second"]
 }
+
+### example with loop
 
 module "myresource-1" {
   for_each      = toset(var.vpc_prefixes)
