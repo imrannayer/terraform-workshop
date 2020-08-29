1c34da87f46674be7cc1761f25da9271d2a2ffba
Syed I Nayer <imrannayer@users.noreply.github.com>
2020-08-29 08:14:35 -0500

    updated registery module lab

--- Azure/Labs/06-modules/registry/main.tf
+++ Azure/Labs/06-modules/registry/main.tf
@@ -6,17 +6,18 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = "my-tf-rg"
   location = "southcentralus"
 }
-# https://registry.terraform.io/modules/Azure/network/azurerm/2.0.0
+
+# https://registry.terraform.io/modules/Azure/vnet/azurerm/2.1.0
+
 module "network" {
-    source              = "Azure/network/azurerm"
-    version             = "~> 3.1"
-    location            = "southcentralus"
-    resource_group_name = azurerm_resource_group.test.name
-    address_space       = "10.0.0.0/16"
-    subnet_names        = ["mySubnet1", "mySubnet2"]
-    subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
+  source              = "Azure/vnet/azurerm"
+  version             = "~> 2.1"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = "10.0.0.0/16"
+  subnet_names        = ["mySubnet1", "mySubnet2"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
